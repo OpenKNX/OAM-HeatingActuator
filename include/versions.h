3b8f0d77ea10c5b860b4390160393b95663be2b0
Andreas Breitschopp <a-breitschopp@ab-tools.com>
2025-01-17 19:58:07 +0100

    Migrated to GPIOModule.

--- include/versions.h
+++ include/versions.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define MAIN_Version "ae4618d"
+#define MAIN_Version "e0d005d"
 #define KNX_Version "2.1.2+764900e"
 #define MODULE_LogicModule_Version "3.5.2+9a1cfe1"
 #define MODULE_LogicModule_Version_Major 3
@@ -12,11 +12,16 @@
 #define MODULE_Common_Version_Minor 2
 #define MODULE_Common_Version_Revision 0
 #define MODULE_Common_ETS 18
-#define MODULE_HeatingActuator_Version "0.1.0+d73ae29"
+#define MODULE_HeatingActuator_Version "0.1.0+5c2c987"
 #define MODULE_HeatingActuator_Version_Major 0
 #define MODULE_HeatingActuator_Version_Minor 1
 #define MODULE_HeatingActuator_Version_Revision 0
 #define MODULE_HeatingActuator_ETS 1
+#define MODULE_GPIOModule_Version "0.0.0+39b2592"
+#define MODULE_GPIOModule_Version_Major 0
+#define MODULE_GPIOModule_Version_Minor 0
+#define MODULE_GPIOModule_Version_Revision 0
+#define MODULE_GPIOModule_ETS 0
 #define MODULE_FileTransferModule_Version "0.1.0+2669301"
 #define MODULE_FileTransferModule_Version_Major 0
 #define MODULE_FileTransferModule_Version_Minor 1
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "Logic.h"
 #include "HeatingActuatorModule.h"
 #include "FileTransferModule.h"
+#include "GPIOModule.h"
 #include "OpenKNX.h"
 #include "hardware.h"
 
@@ -12,6 +13,7 @@ void setup()
     openknx.addModule(1, openknxLogic);
     openknx.addModule(2, openknxHeatingActuatorModule);
     openknx.addModule(3, openknxFileTransferModule);
+    openknx.addModule(4, openknxGPIOModule);
     openknx.setup();
 }
 
