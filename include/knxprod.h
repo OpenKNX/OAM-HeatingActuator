5d36980e8bcafab10b3312b4ea38c939793590f9
Andreas Breitschopp <a-breitschopp@ab-tools.com>
2025-03-04 17:12:30 +0100

    Updated OpenKNX "OGM-Common" to "v1dev" state.

--- include/knxprod.h
+++ include/knxprod.h
@@ -10,11 +10,11 @@
                                              
 #define MAIN_OpenKnxId 0xAF
 #define MAIN_ApplicationNumber 6
-#define MAIN_ApplicationVersion 2
+#define MAIN_ApplicationVersion 3
 #define MAIN_ParameterSize 4318
 #define MAIN_MaxKoNumber 613
 #define MAIN_OrderNumber "OpenKnxHeatingActuator"
-#define BASE_ModuleVersion 18
+#define BASE_ModuleVersion 19
 #define UCT_ModuleVersion 2
 #define HTA_ModuleVersion 1
 #define LOG_ModuleVersion 53
@@ -71,6 +71,9 @@
 #define BASE_HeartbeatExtended                   14      // 1 Bit, Bit 4
 #define     BASE_HeartbeatExtendedMask 0x10
 #define     BASE_HeartbeatExtendedShift 4
+#define BASE_InternalTime                        14      // 1 Bit, Bit 3
+#define     BASE_InternalTimeMask 0x08
+#define     BASE_InternalTimeShift 3
 #define BASE_ManualSave                          14      // 3 Bits, Bit 2-0
 #define     BASE_ManualSaveMask 0x07
 #define     BASE_ManualSaveShift 0
@@ -117,6 +120,8 @@
 #define ParamBASE_ReadTimeDate                        ((bool)(knx.paramByte(BASE_ReadTimeDate) & BASE_ReadTimeDateMask))
 // Erweitertes "In Betrieb"
 #define ParamBASE_HeartbeatExtended                   ((bool)(knx.paramByte(BASE_HeartbeatExtended) & BASE_HeartbeatExtendedMask))
+// InternalTime
+#define ParamBASE_InternalTime                        ((bool)(knx.paramByte(BASE_InternalTime) & BASE_InternalTimeMask))
 // Manuelles speichern
 #define ParamBASE_ManualSave                          (knx.paramByte(BASE_ManualSave) & BASE_ManualSaveMask)
 // Zyklisches speichern
@@ -130,6 +135,7 @@
 #define BASE_KoDiagnose 7
 #define BASE_KoIsSummertime 10
 #define BASE_KoManualSave 11
+#define BASE_KoDateTime 12
 
 // In Betrieb
 #define KoBASE_Heartbeat                           (knx.getGroupObject(BASE_KoHeartbeat))
@@ -143,6 +149,8 @@
 #define KoBASE_IsSummertime                        (knx.getGroupObject(BASE_KoIsSummertime))
 // Speichern
 #define KoBASE_ManualSave                          (knx.getGroupObject(BASE_KoManualSave))
+// Uhrzeit/Datum
+#define KoBASE_DateTime                            (knx.getGroupObject(BASE_KoDateTime))
 
 
 
--- include/versions.h
+++ include/versions.h
@@ -1,22 +1,27 @@
 #pragma once
 
-#define MAIN_Version "e1cc898"
+#define MAIN_Version "84c85c9"
 #define KNX_Version "2.1.2+764900e"
 #define MODULE_LogicModule_Version "3.5.2+9a1cfe1"
 #define MODULE_LogicModule_Version_Major 3
 #define MODULE_LogicModule_Version_Minor 5
 #define MODULE_LogicModule_Version_Revision 2
 #define MODULE_LogicModule_ETS 53
-#define MODULE_Common_Version "1.2.0+9f24e6e"
+#define MODULE_Common_Version "1.3.0+84c4db5"
 #define MODULE_Common_Version_Major 1
-#define MODULE_Common_Version_Minor 2
+#define MODULE_Common_Version_Minor 3
 #define MODULE_Common_Version_Revision 0
-#define MODULE_Common_ETS 18
-#define MODULE_HeatingActuator_Version "0.1.0+11a2956"
+#define MODULE_Common_ETS 19
+#define MODULE_HeatingActuator_Version "0.1.0+456e5c1"
 #define MODULE_HeatingActuator_Version_Major 0
 #define MODULE_HeatingActuator_Version_Minor 1
 #define MODULE_HeatingActuator_Version_Revision 0
 #define MODULE_HeatingActuator_ETS 1
+#define MODULE_GPIOModule_Version "0.0.0+2b8c0a8"
+#define MODULE_GPIOModule_Version_Major 0
+#define MODULE_GPIOModule_Version_Minor 0
+#define MODULE_GPIOModule_Version_Revision 0
+#define MODULE_GPIOModule_ETS 0
 #define MODULE_FileTransferModule_Version "0.1.0+2669301"
 #define MODULE_FileTransferModule_Version_Major 0
 #define MODULE_FileTransferModule_Version_Minor 1
